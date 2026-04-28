93630cec90285b3a4849e52ce7b4d6a8f38881c1
devin hart <devohart@gmail.com>
2026-04-28 11:15:57 -0400

    update audit URL

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,6 +1,6 @@
 let allPageData = [];
 
-const PI_API_URL = "https://your-public-tunnel-url.com/api/audit";
+const PI_API_URL = "https://aud-two-strange-cdt.trycloudflare.com/api/audit";
 
 async function initDashboard() {
   const status = document.getElementById("status");
